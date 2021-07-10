2b6ff5a4cfc37b98fe451674ccd6701eedcbe42f
unknown <khpark1810@gmail.com>
2021-07-10 22:34:26 +0900

    #4.3 File URL and Nweet

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -6,7 +6,6 @@ const Nweet = ({ nweetObj, isOwner }) => {
   const [newNweet, setNewNweet] = useState(nweetObj.text);
   const onDeleteClick = async () => {
     const ok = window.confirm("Are you sure you want to delete this nweet?");
-    console.log(ok);
     if (ok) {
       await dbService.doc(`nweets/${nweetObj.id}`).delete();
     }
@@ -43,6 +42,7 @@ const Nweet = ({ nweetObj, isOwner }) => {
         <>
           {" "}
           <h4>{nweetObj.text}</h4>
+          {nweetObj.attachmentUrl && <img src={nweetObj.attachmentUrl} width="50px" height="50px"/>}
           {isOwner && (
             <>
               <button onClick={onDeleteClick}>Delete Nweet</button>
